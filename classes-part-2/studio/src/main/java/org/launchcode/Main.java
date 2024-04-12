a6852da2baeb5b0a492bec78ec06993500ba58bf
gtracey24 <146043771+cptnez@users.noreply.github.com>
2024-04-12 13:23:01 -0500

    Update Main.java

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,36 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+	// write your code here
+        MenuItem itemOne = new MenuItem("Hot Dog", 2.99, "It's a tasty dot hog!",
+                "Main Course", 1);
+        MenuItem itemTwo = new MenuItem("Corn Nuggets", .99, "Just the way momma made them!",
+                "Side", 2);
+        MenuItem itemThree = new MenuItem("Ice Cream", .99, "It's icy AND creamy!",
+                "Dessert", 3);
+        MenuItem itemFour = new MenuItem("Chicken Sandwich", 3.99, "It's chicken...on a sandwich!",
+                "Main Course", 3);
+
+        Menu menu = new Menu();
+
+        menu.addItem(itemOne);
+        menu.addItem(itemTwo);
+        menu.addItem(itemFour);
+//
+//        menu.removeItem(itemTwo);
+//
+        System.out.println(menu);
+
+        menu.addItem(itemThree);
+
+        System.out.println(menu);
+
+
     }
 }
